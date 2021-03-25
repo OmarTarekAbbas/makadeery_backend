a1b688c1556c4c40f1c9b1e7f992f0241b85a30c
mahmoud maher <mahmoud.maher@ivas.com.eg>
2021-03-25 16:15:34 +0200

    update

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -7,10 +7,11 @@
         <div class="col-12">
             <h4 class="ramdan_menu text-center text-capitalize rounded">منيو رمضان</h5>
         </div>
+
         @foreach ($categorys as $category)
-        <div class="col-6">
+        <div class="{{$loop->last  % 2==1 ? 'col-12' : 'col-6'}}">
             <a href="category.php">
-                <div class="ramdan_category">
+                <div class="ramdan_category {{$loop->last % 2==1 ? 'ramdan_category_last' : ''}}">
                     <img class="ramdan_category_img" src="{{$category->image}}" alt="{{$category->title}}">
 
                     <p class="ramdan_category_title text-center text-capitalize">{{$category->title}}</p>
