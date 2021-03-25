319ea894b9a7623f145d44955afdffa2577ef20e
omar tarek <omar.tarek@iVAS.local>
2021-03-25 18:18:22 +0200

    make front

--- app/Http/Filters/ContentFilters/GlobalSearchFilter.php
+++ app/Http/Filters/ContentFilters/GlobalSearchFilter.php
@@ -16,13 +16,6 @@ class GlobalSearchFilter implements Filter
      */
     public function apply(Builder $builder, $value)
     {
-        return $builder->join('translatables','translatables.record_id','=','contents.id')
-        ->join('tans_bodies','tans_bodies.translatable_id','translatables.id')
-        ->where('translatables.table_name','contents')
-        ->where('translatables.column_name','title')
-        ->where(function($q) use ($value){
-            $q->where('contents.title', 'like', '%' . $value. '%');
-            $q->orWhere('tans_bodies.body', 'like', '%' . $value . '%');
-        });
+        return $builder->where('contents.title', 'like', '%' . $value. '%');
     }
 }
