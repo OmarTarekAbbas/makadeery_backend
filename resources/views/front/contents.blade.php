2e2c092edfae1449addd12de8f7ee54c4c946760
Baher Talal <57946827+baher-talal@users.noreply.github.com>
2021-03-28 14:36:53 +0200

    ipad

--- resources/views/front/contents.blade.php
+++ resources/views/front/contents.blade.php
@@ -3,13 +3,13 @@
 <!-- End Header -->
 
 <section class="sub_category">
-  <div class="row m-0 w-100">
+  <div class="row m-0 w-100 justify-content-center">
     <div class="col-12">
       <h3 class="ramdan_menu text-center text-capitalize rounded">اختر مقاديرك</h5>
     </div>
 
     @forelse ($contents as $content)
-    <div class="col-6">
+    <div class="col-md-4 col-6">
       <a href="{{route('meal',$content->id)}}" class="link_href">
         <div class="ramdan_category">
           <p class="ramdan_category_desc mb-0">{{$content->title}}</p>
