f3ec6f01e08a7c79b6326e3e738c04284db8b10c
Baher Talal <57946827+baher-talal@users.noreply.github.com>
2021-03-29 13:56:41 +0200

    laravel mix webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,17 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+/* mix.js('resources/js/app.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css'); */
+
+mix.styles([
+  'front/css/all.min.css',
+  'front/css/bootstrap.min.css',
+  'front/css/style_ar.css'
+], 'front/css/all_css_minify.css');
+
+mix.scripts([
+  'front/js/jquery-3.3.1.min.js',
+  'front/js/popper.min.js',
+  'front/js/scribt.js'
+], 'front/js/all_script_minify.js');
