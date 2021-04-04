a7ffa2ed5e8d4538c5955a040a081b0f57e743c9
omar tarek <omar.tarek@iVAS.local>
2021-04-04 17:08:11 +0200

    make 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,33 @@
+<!-- Start Header -->
+@include("front.header")
+<!-- End Header -->
+
+<style>
+  .main .sub_category .ramdan_menu {
+    display: none;
+  }
+</style>
+
+<div class="container">
+  <section id="error">
+    <div class="row">
+      <div class="col-12">
+        <div class="card text-center p-5 shadow-lg">
+
+          <span><i class="far fa-5x fa-frown"></i></span>
+
+          <p class="mt-2 text-capitalize"></p>
+
+          <h5 class="mb-3 font-weight-bold text-capitalize">لا يوجود وجبات</h5>
+
+          <a href="{{route('home')}}" class="btn btn-block rounded-pill text-capitalize link_href">اذهب إلى
+            الصفحة الرئيسية</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+<!-- Start Footer -->
+@include("front.footer")
+<!-- End Footer -->
