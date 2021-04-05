a60f885dfc39103afd5240f3349b4673ae882b88
mohamed mahmoud <m.mahmoud@iVAS.local>
2021-04-05 12:49:24 +0200

    make 404 one page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,12 +2,6 @@
 @include("front.header")
 <!-- End Header -->
 
-<style>
-  .main .sub_category .ramdan_menu {
-    display: none;
-  }
-</style>
-
 @include("front.error404")
 
 <!-- Start Footer -->
