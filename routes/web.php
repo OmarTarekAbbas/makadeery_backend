e6b0a8b10a30b5239a6b11a5b4015217cefceb64
mahmoud maher <mahmoud.maher@ivas.com.eg>
2021-04-12 16:34:34 +0200

    ramadan update website - add home middleware

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,13 @@
 get_static_routes() ;
 get_dynamic_routes();
 
-Route::get("/", "FrontController@home")->name('home');
-Route::get("{category}/category/{category_title?}", "FrontController@subcategory")->name('subcategory');
-Route::get("{category_id?}/contents/{category_title?}/{subcategory_title?}", "FrontController@listContents")->name('contents');
-Route::get("contents", "FrontController@listContents")->name('search');
-Route::get("{content}/meal", "FrontController@meal")->middleware('front')->name('meal');
+Route::middleware(['front'])->group(function () {
+  Route::get("/", "FrontController@home")->name('home');
+  Route::get("{category}/category/{category_title?}", "FrontController@subcategory")->name('subcategory');
+  Route::get("{category_id?}/contents/{category_title?}/{subcategory_title?}", "FrontController@listContents")->name('contents');
+  Route::get("contents", "FrontController@listContents")->name('search');
+  Route::get("{content}/meal", "FrontController@meal")->name('meal');
+});
 
 
 
